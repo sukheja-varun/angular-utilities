52752e93d1153cbcc457ac07c0078c1045dd862c
Varun Sukheja <varun.sukheja@myglamm.com>
2020-05-02 12:28:07 +0530

    added new service check-internet for checking net connectivity. Then implemented this service to no-internet component

--- src/app/components/no-internet/no-internet.component.ts
+++ src/app/components/no-internet/no-internet.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { CheckInternetService } from '@service/check-internet.service';
+
 
 @Component({
   selector: 'vs-no-internet',
   templateUrl: './no-internet.component.html',
   styleUrls: ['./no-internet.component.scss']
 })
-export class NoInternetComponent implements OnInit {
+export class NoInternetComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  connectivitySubs$: Subscription;
+  isInternetActive = false;
 
-  ngOnInit(): void {
+  constructor(private _checkInternet: CheckInternetService) { }
+
+  ngOnInit() {
+    this.connectivitySubs$ = this._checkInternet
+      .checkConnectivity()
+      .subscribe((isOnline) => {
+        console.log('isOnline', isOnline);
+        this.isInternetActive = isOnline;
+      });
   }
 
+  ngOnDestroy(): void {
+    this.connectivitySubs$.unsubscribe();
+  }
 }
