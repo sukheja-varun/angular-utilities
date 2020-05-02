064fa0fbabcace90dde3b90af232898c415e8581
Varun Sukheja <varun.sukheja@myglamm.com>
2020-05-02 11:37:37 +0530

    added home component and a blank no-internet component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { NoInternetComponent } from './components/no-internet/no-internet.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'no-internet', component: NoInternetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
