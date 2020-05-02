d135f1de125b2f5e12477e510fb5b446af1578f6
Varun Sukheja <varun.sukheja@myglamm.com>
2020-05-02 16:50:24 +0530

    added new library ngx-highlight for displaying code snippets in HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoInternetComponent } from './components/no-internet/no-internet.component';
 import { HomeComponent } from './components/home/home.component';
 import { DateExtensionComponent } from './components/date-extension/date-extension.component';
+
+/**
+ * Import specific languages to avoid importing everything
+ * The following will lazy load highlight.js core script (~9.6KB) + the selected languages bundle (each lang. ~1kb)
+ */
+export function getHighlightLanguages() {
+  return {
+    typescript: () => import('highlight.js/lib/languages/typescript'),
+    css: () => import('highlight.js/lib/languages/css'),
+  };
+}
 
 @NgModule({
   declarations: [
@@ -16,9 +29,17 @@ import { DateExtensionComponent } from './components/date-extension/date-extensi
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HighlightModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        languages: getHighlightLanguages()
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
